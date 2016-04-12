adaad48c6f8f184b4d2204090ec2406308033977
mcgeehancoding <mmcgeehan@paduaacademy.org>
2016-04-11 20:11:20 -0400

    Added files via upload

--- index.php
+++ index.php
@@ -1,6 +1,25 @@
 <?php
 	/*
-	function greet(){
+	
+	“No amount of experimentation can ever prove me right; a single experiment can prove me wrong.” - Einstein
+	
+	Let's download  AMPPS. Google 'Ampps' and you'll find a software stack from softaculous that allows you to use server side languages, set up databases, and develop your websites using open source software. Wow! 
+	
+	1. Start Apache and MySQL from the Control Center....
+	2. In the browser, visit localhost/ampps/
+	3. Add Domain
+	4. Try php.dev
+	
+	Alright, now we have the folder set up. Let's add this file to the correct foler. 
+	
+	5. Finder, Applications, Ampps, www....
+	6. Now check for php.dev.... Add it if it's not there....
+	7. Delete everything in and around that folder....
+	8. Add this file as index.php!
+	
+	Now go back to the browser and visit php.dev!
+	
+		function greet(){
 		echo 'Hello World';
 		}
 		
