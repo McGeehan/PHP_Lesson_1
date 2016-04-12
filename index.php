a9ce78070858ff1393a75435dec2585667b413d2
mcgeehancoding <mmcgeehan@paduaacademy.org>
2016-04-11 20:05:19 -0400

    Lesson_Version_1.0
    Lesson_Version_1.0

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+	/*
+	function greet(){
+		echo 'Hello World';
+		}
+		
+	function greet($name){
+		echo 'Hello '. $name;
+		}
+		
+	function greet($greeting, $name='McGee'){
+		echo alpha2($greeting.'  '.$name);
+		}
+		
+	 greet('Whats Up', 'McGee');
+	 */
+	 
+	 $num = 4815162342;
+	 
+	 if($num != 10) {
+	 	echo 'Correct';
+	 	}
+?>
